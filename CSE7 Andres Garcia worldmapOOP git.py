4c41e47d6c0140fc6d749d3d68b63c21452c7554
kingkong1001 <568412g@gmail.com>
2017-02-23 15:21:54 -0800

    Initial Upload
    2.23.17

---
+++ CSE7 Andres Garcia worldmapOOP git.py
@@ -0,0 +1,14 @@
+class Room(object):
+    def __init__(self, name, north, south, east, west, up, down, desc):
+        self.name = name
+        self.description = desc
+        self.north = north
+        self.south = south
+        self.east = east
+        self.west = west
+        self.up = up
+        self.down = down
+    def move(self, direction):
+        global node
+        node = globals()[getattr(self, direction)]
+
